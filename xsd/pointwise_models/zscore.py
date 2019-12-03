85bede2e0407ba278693b6a68057f893dcde1363
Julia Kent <jkent@ucar.edu>
2019-12-03 08:42:50 -0700

    Corrected Rolling in Fit

--- xsd/pointwise_models/zscore.py
+++ xsd/pointwise_models/zscore.py
@@ -2,6 +2,7 @@ from sklearn.base import RegressorMixin
 from sklearn.linear_model.base import LinearModel
 import pandas as pd
 import numpy as np
+import xarray as xr
 
 class ZScoreRegressor(LinearModel, RegressorMixin):
     """ Z Score Regressor bias correction model wrapper
@@ -32,11 +33,11 @@ class ZScoreRegressor(LinearModel, RegressorMixin):
         -------
         self : returns an instance of self.
         """
+
+        X_mean, X_std = _calc_stats(X, self.window_width)
+        y_mean, y_std  = _calc_stats(y, self.window_width)
         
-        X_mean, X_std, X_zscore = _get_circ_stats(X, self.window_width)
-        y_mean, y_std, y_zscore = _get_circ_stats(y, self.window_width)
-        
-        shift, scale = _calc_correction_params(X_mean, X_std, y_mean, y_std)
+        shift, scale = _get_params(X_mean, X_std, y_mean, y_std)
 
         self.shift = shift
         self.scale = scale
@@ -73,79 +74,59 @@ class ZScoreRegressor(LinearModel, RegressorMixin):
 
 #Helpers
 # Helpers for Fit
-def _hack_circular_rolling(df, window_width):
+
+def _reshape(ds, window_width):
     """
-    Helper function for `fit` that creates a bookended average year 
+    Helper function for `fit` that splits the year and day
+    dimensions of the time-coordinate and bookends the years
     e.g. (Dec15:31 + whole year + Jan1:15) if window_width is 31 days.
-    This is to get average values for each day across all years, 
-    and so that the rolling function wraps around 
-    (Jan 1st rolling stats are informed by late December)
+    ---------------------
+    Parameters:
+    ds : Xarray Dataset
+    window_width: the size of the rolling window.
+
+    Returns:
+    ds_rsh : Reshaped Xarray dataset
+    """
+    split = lambda g: (g.rename({'time': 'day'})
+                       .assign_coords(day=g.time.dt.dayofyear.values))
+    ds_split = ds.groupby('time.year').apply(split)
+    
+    early_Jans = ds_split.isel(day = slice(None,window_width//2))
+    late_Decs = ds_split.isel(day = slice(-window_width//2,None))
+    
+    ds_rsh = xr.concat([late_Decs,ds_split,early_Jans],dim='day')
+    return ds_rsh
+
+def _calc_stats(df, window_width):
+    """
+    Helper function for `fit` that calculates the rolling mean and  
+    standard deviation for each day of the year across all years.
     ---------------------
     Parameters:
     df : Pandas dataframe
     window_width: the size of the rolling window.
 
     Returns:
-    df_avyear : Average values for each day across all years
-    df_circ: df_avyear with bookends
+    df_mean : Means for each day of year across all years
+    df_std:  Standard deviations for each day of year across all years
     """
-    df_avyear = df.groupby(lambda x: x.dayofyear).mean()
+    
+    ds = df.to_xarray()
+    ds_rsh = _reshape(ds, window_width)
+    
+    ds_rolled = ds_rsh.rolling(day=window_width, center=True).construct('win_day')
+    
+    n = window_width//2+1
+    ds_mean = ds_rolled.mean(dim=['year','win_day']).isel(day=slice(n,-n))
+    ds_std = ds_rolled.std(dim=['year','win_day']).isel(day=slice(n,-n))
 
-    bookend = int(window_width-1)/2
-    last = df_avyear.iloc[-bookend:]
-    first = df_avyear.iloc[:bookend]
-
-    df_circ = pd.concat([last, df_avyear, first], ignore_index=True)
-    return df_avyear, df_circ;
-
-        
-def _calc_circ_stats(df_avyear, df_circ, window_width): 
-    """
-    Helper function for `fit` that calculates statistics (mean, std, and zscore) 
-    on a bookended average year for each day using a moving window.
-    ---------------------
-    Parameters:
-    df_avyear : Average values for each day across all years
-    df_circ: df_avyear with bookends
-    window_width: the size of the rolling window.
-
-    Returns:
-    circ_mean : Mean calculated using the moving window 
-        for each day on an average year
-    circ_std : Standard deviation calculated using the 
-        moving window for each day on an average year
-    """
-    bookend = int(window_width-1)/2
-    df_mean = df_circ.rolling(window_width, center=True).mean()
-    circ_mean = df_mean.iloc[bookend:365+bookend]
-
-    df_std = df_circ.rolling(window_width, center=True).std()
-    circ_std = df_std.iloc[bookend:365+bookend]
-    return circ_mean, circ_std;
-
-        
-def _get_circ_stats(df, window_width):
-    """
-    Helper function for `fit` that generates bookended average
-    year and calculates statistics (mean, std, and zscore) 
-    on that bookended average year for each day using a moving window.
-    ---------------------
-    Parameters:
-    df : Pandas dataframe
-    window_width: the size of the rolling window.
-
-    Returns:
-    circ_mean : Mean calculated using the moving window 
-        for each day on an average year
-    circ_std : Standard deviation calculated using the 
-        moving window for each day on an average year
-    """
-    df_av, df_circ = _hack_circular_rolling(df, window_width)
-    circ_mean, circ_std = _calc_circ_stats(df_av, df_circ, window_width)
-    return circ_mean, circ_std
+    df_mean = ds_mean.to_dataframe()
+    df_std = ds_std.to_dataframe()
+    return df_mean, df_std
                      
 
-def _calc_correction_params(hist_mean, hist_std, meas_mean, meas_std):   
+def _get_params(hist_mean, hist_std, meas_mean, meas_std):   
     """
     Helper function for `fit` that calculates the shift and scale parameters
     for z-score correction by comparing the historical and measured
