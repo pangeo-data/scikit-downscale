e50025231f14e02dbe54b8e046ba78a9f4e049ec
marcbour <bourgault.marc-andre@ouranos.ca>
2020-01-13 15:20:27 -0500

    trying detrend function

--- xsd/test/test_MAB.py
+++ xsd/test/test_MAB.py
@@ -1,13 +1,15 @@
-import xsd.qm
+import xsd.xsd.qm
 import xarray as xr
 from pathlib import Path
 
 path = Path('/home/david/projects/test_xsd_MAB/')
-name_fut = path / "tasmax_RACMO22T_AFR-44_MOHC-HadGEM2-ES_rcp26_4qqmap.nc"
-name_ref = path / "tasmax_RACMO22T_AFR-44_MOHC-HadGEM2-ES_rcp26_ref_4qqmap.nc"
-name_obs = path / "tasmax_farakoba.nc"
+path = Path("/exec/marcbour/sim_climat/burkina/pcci/leo/regrid/tasmin/")
+name_fut = path / "tasmin_RACMO22T_AFR-44_MOHC-HadGEM2-ES_rcp26_4qqmap.nc"
+name_ref = path / "tasmin_RACMO22T_AFR-44_MOHC-HadGEM2-ES_rcp26_ref_4qqmap.nc"
+name_obs = path / "tasmin_leo.nc"
+name_obs = "/exec/marcbour/sim_climat/burkina/pcci/leo/obs/"+"tasmin_leo.nc"
 
-v = "tasmax"
+v = "tasmin"
 
 fut = xr.open_dataset(name_fut).squeeze()[v]
 ref = xr.open_dataset(name_ref).squeeze()[v]
@@ -23,10 +25,15 @@ else:
     obs = obs.interpolate_na(dim="time")
     kind = "+"
 
-nq = 20
-group = 'time.dayofyear'  # 'time.dayofyear'
-detrend_order = 4
+nq = 50
+group = 'time.month'  # 'time.dayofyear'
+detrend_order = 1
 
-qmf = xsd.qm.train(ref, obs, nq, group, kind=kind, detrend_order=detrend_order)
+qmf = train(ref, obs, nq, group, kind=kind, detrend_order=detrend_order)
+pp_fut = predict(fut, qmf, interp=True, detrend_order=detrend_order)
 
-pp_fut = xsd.qm.predict(fut, qmf, interp=True, detrend_order=detrend_order)
+pp_fut.plot()
+ref.plot(alpha=0.5)
+obs.plot(alpha=0.5)
+
+plt.legend(["qqmap","ref","obs"])
