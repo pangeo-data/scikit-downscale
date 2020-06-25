1f589854dbc750b065e4e285ee11797ece4d7627
Diana Gergel <dgergel@gmail.com>
2020-06-25 00:27:13 +0000

    fix isort error

--- skdownscale/pointwise_models/__init__.py
+++ skdownscale/pointwise_models/__init__.py
@@ -1,6 +1,6 @@
 from .bcsd import BcsdPrecipitation, BcsdTemperature
 from .core import PointWiseDownscaler
 from .gard import AnalogRegression, PureAnalog
+from .groupers import PaddedDOYGrouper
 from .utils import LinearTrendTransformer, QuantileMapper
 from .zscore import ZScoreRegressor
-from .groupers import PaddedDOYGrouper
--- skdownscale/pointwise_models/bcsd.py
+++ skdownscale/pointwise_models/bcsd.py
@@ -7,8 +7,8 @@ from sklearn.linear_model.base import LinearModel
 from sklearn.utils.validation import check_is_fitted
 
 from .base import AbstractDownscaler
-from .utils import QuantileMapper, ensure_samples_features, check_datetime_index
 from .groupers import PaddedDOYGrouper
+from .utils import QuantileMapper, check_datetime_index, ensure_samples_features
 
 
 def MONTH_GROUPER(x):
