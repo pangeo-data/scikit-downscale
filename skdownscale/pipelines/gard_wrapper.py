3197021fff015d97c30caadda4072ee49d66c140
tcchiao <tcchiao@gmail.com>
2021-10-27 22:18:58 +0000

    wip

--- skdownscale/pipelines/gard_wrapper.py
+++ skdownscale/pipelines/gard_wrapper.py
@@ -5,30 +5,39 @@ from sklearn.preprocessing import QuantileTransformer, StandardScaler
 from skdownscale.pointwise_models.utils import default_none_kwargs
 
 
+
+
+
 class GardWrapper:
     def __init__(
         self, 
         model, 
+        label_name,
         feature_list=None, 
         dim='time', 
         bias_correction_method='quantile_transform', 
         bc_kwargs=None,
-        spatial_features=(1, 1)
+        generate_scrf=True,
+        # spatial_features=(1, 1)
         ):
         """
         Parameters
         ----------
-        model: a GARD model instance to be fitted pointwise 
-        feature_list: a list of feature names to be used in predicting 
-        dim: dimension to apply the model along. Default is ``time``. 
+        model                 : a GARD model instance to be fitted pointwise 
+        feature_list          : a list of feature names to be used in predicting 
+        dim                   : string. dimension to apply the model along. Default is ``time``. 
         bias_correction_method: string of the name of bias correction model 
-        bc_kwargs: kwargs directly passed to the bias correction model
-        spatial 
+        bc_kwargs             : kwargs dict. directly passed to the bias correction model
+        generate_scrf         : boolean. indicates whether a spatio-temporal correlated random field (scrf) will be 
+                                generated based on the fine resolution data provided in .fit as y. if false, it is 
+                                assumed that a pre-generated scrf will be passed into .predict as an argument that 
+                                matches the prediction result dimensions.  
         """
         self._dim = dim
         if not isinstance(model, (AnalogBase, PureRegression)):
             raise TypeError('model must be part of the GARD family of pointwise models ')
         self.features = feature_list
+        self.label_name = label_name
         self._model = model 
 
         # TODO: allow different methods for each feature 
@@ -37,7 +46,7 @@ class GardWrapper:
             raise NotImplementedError(f'bias correction method must be one of {availalbe_methods}')
         self.bias_correction_method = bias_correction_method
         self.bc_kwargs = bc_kwargs
-
+        self.generate_scrf = generate_scrf
 
     def fit(self, X, y, **kwargs):
         """
@@ -70,6 +79,10 @@ class GardWrapper:
 
         #TODO: spatial features 
         #TODO: extend this to include transforming the feature space into PCA, etc 
+
+        # generate correlation lengths for spatio-temporal correlated random field (scrf) if needed 
+        if self.generate_scrf:
+            self.len_scales = find_correlation_length_scale(data=y) 
 
         # point wise transformation on X based on what bias correction method is chosen 
         bc_kws = default_none_kwargs(self.bc_kwargs, copy=True)
@@ -150,3 +163,33 @@ class GardWrapper:
     def _postprocess(self, y_hat):
         # TODO: any postprocessing needed for gard? 
         return y_hat
+
+
+    def find_correlation_length_scale(self, data, seasonality_period=31):
+        for d in ['time', 'lat', 'lon']:
+            assert d in data
+
+        # remove seasonality before finding correlation length, otherwise the seasonality correlation dominates 
+        seasonality = data.rolling({'time': seasonality_period}, center=True, min_periods=1).mean().groupby('time.dayofyear').mean()
+        detrended = data.groupby("time.dayofyear") - seasonality
+
+        # find spatial length scale 
+        bin_center, gamma = gs.vario_estimate(pos=(detrended.lon.values, detrended.lat.values), field=detrended[self.label_name].values, latlon=True, mesh_type='structured')
+        spatial = gs.Gaussian(dim=2, latlon=True, rescale=gs.EARTH_RADIUS)
+        spatial.fit_variogram(bin_center, gamma, sill=np.mean(np.var(fields, axis=(1,2))))
+        spatial_len_scale = spatial.len_scale
+
+        # find temporal length scale 
+        # break the time series into fields of 1 year length, since otherwise the algorithm struggles to find the correct length scale 
+        fields = []
+        day_in_year = 365
+        for yr, group in detrended[self.label_name].groupby('time.year'):
+            v = group.isel(time=slice(0, day_in_year)).stack(point=['lat', 'lon']).transpose('point', 'time').values
+            fields.extend(list(v))
+        t = np.arange(day_in_year) / 1000.
+        bin_center, gamma = gs.vario_estimate(pos=t, field=fields, mesh_type='structured')
+        temporal = gs.Gaussian(dim=1)
+        temporal.fit_variogram(bin_center, gamma, sill=np.mean(np.var(fields, axis=1)))
+        temporal_len_scale = temporal.len_scale
+
+        return {'temporal': temporal_len_scale, 'spatial': spatial_len_scale}
